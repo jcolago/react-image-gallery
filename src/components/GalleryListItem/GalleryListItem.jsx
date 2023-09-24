a762dbb580e99b249150855ae151d85e5ed25ea9
jcolago <jcolago2005@gmail.com>
2023-09-24 17:22:52 -0500

    Correct description functionallity

--- src/components/GalleryListItem/GalleryListItem.jsx
+++ src/components/GalleryListItem/GalleryListItem.jsx
@@ -1,12 +1,13 @@
+import "./GalleryListItem.css"
 
 //Exports function for use in GallerList and props used. Conditionally renders discription based on discription state
 export default function GalleryListItem({ image, addLike, setShowDescription, showDescription }) {
     return (
         <div className="image-card">
-            <div className="image" ><img src={image.path} onClick={() => setShowDescription(true)} />
+            <div className="image" ><img  src={image.path} onClick={() => setShowDescription(image.id)} />
             </div>
             
-                {showDescription !== null && (
+                {showDescription === image.id && (
                     <div className="description">
                     <p>{image.description}</p><button className="close-description-button" onClick={() => setShowDescription(null)}>Close Description</button>
                     </div>)}
