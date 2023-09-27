36131c906ed4d5292fc331e200345ae8c08c5092
jcolago <jcolago2005@gmail.com>
2023-09-27 14:43:51 -0500

    Updated code in CalleryListItem.jsx

--- src/components/GalleryListItem/GalleryListItem.jsx
+++ src/components/GalleryListItem/GalleryListItem.jsx
@@ -7,12 +7,9 @@ export default function GalleryListItem({ image, addLike }) {
     const [showDescription, setShowDescription] = useState(null);
     return (
         <div className="image-card">
-            {showDescription !== image.id && (
+            {showDescription !== image.id ? (
                 <div className="image" ><img src={image.path} onClick={() => setShowDescription(image.id)} />
-                </div>)}
-
-            {showDescription === image.id && (
-                <div className="description">
+                </div>) : (<div className="description">
                     <p>{image.description}</p><button className="close-description-button" onClick={() => setShowDescription(null)}>Close Description</button>
                 </div>)}
 
